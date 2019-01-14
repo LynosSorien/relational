9d547a14bfb7a2b313c42bd3c5e6d1e677fa9549
David Jorquera <david.jorquera@doubleyou.com>
2019-01-14 12:56:51 +0100

    Started project with Spring initializr

---
+++ src/main/java/com/djorquab/relational/relational/RelationalApplication.java
@@ -0,0 +1,14 @@
+package com.djorquab.relational.relational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalApplication.class, args);
+	}
+
+}
+
